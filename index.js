f5e481ca62fb3cc6ad41e126a844e0071532fe99
Bacon Chinazom Mmakogu <baconchinazom@gmail.com>
2025-04-02 19:29:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require("express")()
-const desiredPath = process.env.DESIRED_PATH || "/Bacon"
+const desiredPath = process.env.DESIRED_PATH || "/"
 const port = process.env.PORT || 80
 const number = process.env.NUMBER || 0
 
 app.get(desiredPath, (req, res) => {
-    res.send(`<h1>Hello from ${desiredPath} number ${number}!</h1>`)
+    res.send(`<h1>Hello from Bacon ${desiredPath} number ${number}!</h1>`)
 })
 
 app.get("/healthcheck", (req, res) => {
